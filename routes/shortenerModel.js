9336750885d02d2d328392ae254922caa14f27f4
fengchen <chen.feng1@husky.neu.edu>
2020-04-17 14:02:17 -0700

    update

--- routes/shortenerModel.js
+++ routes/shortenerModel.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/shortener");
+mongoose.connect("mongodb+srv://fengchen:123@cluster0-0lpae.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true");
 
 
 var urlSchema = new mongoose.Schema({
